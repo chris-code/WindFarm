605c3683ade30331b3485969d7fed6ef2dbf32d3
Christian Mielers <Christian.Mielers@rub.de>
2015-01-11 20:40:20 +0100

    GA3 now supports plus selection.
    And probably lots of other stuff i forgot (sorry).
    Stuff i can think of:
    - Sorting of individuals now done by comparison operator overloading
    - Best individual is always stored throughout the run (not elitism though, it is not put into the new generation, just separately stored).
    - Validity threshold can be set via parameters now.

--- src/GA_3.cpp
+++ src/GA_3.cpp
@@ -2,41 +2,60 @@
 #include <chrono>
 #include "GA_3.h"
 
-GA3::GA3(KusiakLayoutEvaluator &evaluator, long populationSize, long offspringCount, long numberOfTurbines):
+//	======================================== PUBLIC MEMBERS ========================================
+
+GA3::GA3(KusiakLayoutEvaluator &evaluator, bool commaSelection, long populationSize, long offspringCount,
+         long numberOfTurbines, double validityThreshold):
 	wfle(evaluator) {
+	if(commaSelection && offspringCount < populationSize) {
+		cerr << "Genetic Algorithm Instance: ERROR: comma selection requires offspringCount >= populationSize" << endl;
+	}
+
+	this->commaSelection = commaSelection;
 	this->populationSize = populationSize;
 	this->offspringCount = offspringCount;
 	this->numberOfTurbines = numberOfTurbines;
+	this->validityThreshold = validityThreshold;
 
 	long seed = chrono::system_clock::now().time_since_epoch().count();
 	randomEngine = default_random_engine(seed);
+	cout << "Genetic Algorithm Instance: Using seed " << seed << endl;
 
-	for(long l = 0; l < populationSize; ++l) {
-		population.push_back(Individual(&wfle, &randomEngine, generateRandomLayout(), -1.0, Matrix<double>(1, 1)));
+	for(long l = 0; l < offspringCount; ++l) {
+//		Initialize population with offspringCount individuals. Normally, one would initialize it with the
+//		populationSize, but this way we evaluate the same amount of individuals in each iteration (for the sake of
+//		consistency). This only affects the first iteration and is thus only minimally wasteful.
+		population.push_back(Individual(&wfle, &randomEngine, numberOfTurbines, validityThreshold));
 	}
+	best.push_back(population[0]);
 }
 
 void GA3::run() {
-//	TODO shrink population if necessary
 	while(true) {
 		evaluatePopulation();
 		while(long(population.size()) > populationSize) {
 			population.pop_back();
 		}
-		cout << "Fitness after " << wfle.getNumberOfEvaluation() << " evaluations: " << population[0].fitness << endl;
+		if(population[0].fitness > best[0].fitness) {
+			best[0] = population[0];
+		}
+		cout << "Fitness after " << wfle.getNumberOfEvaluation() << " evaluations: " << best[0].fitness;
+		cout << " (" << best[0].countInvalidTurbines() << " invalid turbines)" << endl;
 		vector<long> parentIndices = selectParents();
 		vector<Individual> offspring = generateOffspring(parentIndices);
-		population = offspring;
+
+		if(commaSelection) {
+			population.clear();
+		}
+		population.insert(population.end(), offspring.begin(), offspring.end());
 	}
 }
 
 Matrix<double> GA3::getLayout() {
-	return population[0].layout; // FIXME this should be the best one
+	return best[0].layout;
 }
 
-bool compareIndividuals(const Individual &a, const Individual &b) {
-	return a.fitness > b.fitness;
-}
+//	======================================== PRIVATE MEMBERS ========================================
 
 void GA3::evaluatePopulation() {
 	for(auto & individual : population) {
@@ -44,7 +63,7 @@ void GA3::evaluatePopulation() {
 		individual.fitness = wfle.getWakeFreeRatio();
 		individual.turbineFitnesses = *(wfle.getTurbineFitnesses());
 	}
-	sort(population.begin(), population.end(), compareIndividuals); //TODO overwrite operator<
+	sort(population.begin(), population.end());
 }
 
 vector<long> GA3::selectParents() {
@@ -60,7 +79,6 @@ vector<long> GA3::selectParents() {
 				parentIndices.push_back(l);
 			}
 		}
-		parentIndices.push_back(0); // This should be unreachable
 	}
 	return parentIndices;
 }
@@ -68,60 +86,9 @@ vector<long> GA3::selectParents() {
 vector<Individual> GA3::generateOffspring(vector<long> parentIndices) {
 	vector<Individual> offspring;
 	for(auto p : parentIndices) {
-//		TODO
-		Individual i = population[p].mutate();
+		Individual i = Individual(population[p]);
+		i.mutate();
 		offspring.push_back(i);
 	}
 	return offspring;
-}
-
-Matrix<double> GA3::generateRandomLayout() {
-	uniform_real_distribution<double> randomPosX(0, 0 + wfle.scenario.width);
-	uniform_real_distribution<double> randomPosY(0, 0 + wfle.scenario.height);
-
-	Matrix<double> layout(numberOfTurbines, 2);
-	long placedTurbines = 0;
-	while(placedTurbines < numberOfTurbines) {
-		double posX = randomPosX(randomEngine);
-		double posY = randomPosY(randomEngine);
-
-//		Check if we picked a location within an obstacle or too close to another turbine
-		bool isValidPosition = true;
-		if(isInObstacle(posX, posY)) {
-			isValidPosition = false;
-		}
-		for(long t = 0; t < placedTurbines; ++t) {
-			if(isTooClose(posX, posY, layout.get(t, 0), layout.get(t, 1))) {
-				isValidPosition = false;
-			}
-		}
-
-		if(isValidPosition) {
-			layout.set(placedTurbines, 0, posX);
-			layout.set(placedTurbines, 1, posY);
-			++placedTurbines;
-		}
-	}
-	return layout;
-}
-
-bool GA3::isInObstacle(double posX, double posY) {
-	for(long o = 0; o < wfle.scenario.obstacles.rows; ++o) {
-		double xmin = wfle.scenario.obstacles.get(o, 0);
-		double ymin = wfle.scenario.obstacles.get(o, 1);
-		double xmax = wfle.scenario.obstacles.get(o, 2);
-		double ymax = wfle.scenario.obstacles.get(o, 3);
-		if(posX > xmin && posY > ymin && posX < xmax && posY < ymax) {
-			return true;
-		}
-	}
-	return false;
-}
-
-bool GA3::isTooClose(double posX1, double posY1, double posX2, double posY2) {
-	double distance = sqrt(pow(posX1 - posX2, 2.0) + pow(posY1 - posY2, 2.0));
-	if(distance <= 8 * wfle.scenario.R) {
-		return true;
-	}
-	return false;
 }
--- src/GA_3.h
+++ src/GA_3.h
@@ -10,40 +10,132 @@ class Individual {
 		Matrix<double> layout;
 		double fitness;
 		Matrix<double> turbineFitnesses;
-		Individual(KusiakLayoutEvaluator *evaluator, default_random_engine *engine, Matrix<double> layout,
-		           double fitness, Matrix<double> turbineFitnesses):
+
+		Individual(KusiakLayoutEvaluator *evaluator, default_random_engine *engine, long numberOfTurbines,
+		           double validityThreshold):
 			wfle(evaluator), randomEngine(engine) {
-			this->layout = layout;
-			this->fitness = fitness;
-			this->turbineFitnesses = turbineFitnesses;
+			this->layout = Matrix<double>(numberOfTurbines, 2);
+			this->fitness = -1.0;
+			this->turbineFitnesses = Matrix<double>();
+			this->validityThreshold = validityThreshold;
+
+			initializeLayoutRandomly();
 		}
-		Individual mutate() {
-//			TODO
+		bool operator<(const Individual &other) const { // Required for sorting
+			return fitness > other.fitness;
+		}
+		void mutate() {
+			double sigma = 140.0;
+			normal_distribution<double> turbineOffset(0, sigma);
+			for(long t = 0; t < layout.rows; ++t) {
+				if(turbineFitnesses.get(t, 0) >= validityThreshold)
+					continue;
+
+				double newPosX = layout.get(t, 0) + turbineOffset(*randomEngine);
+				double newPosY = layout.get(t, 1) + turbineOffset(*randomEngine);
+				while(! isValidMutation(t, newPosX, newPosY)) {
+					newPosX = layout.get(t, 0) + turbineOffset(*randomEngine);
+					newPosY = layout.get(t, 1) + turbineOffset(*randomEngine);
+				}
+				layout.set(t, 0, newPosX);
+				layout.set(t, 1, newPosY);
+			}
+		}
+		long countInvalidTurbines() {
+			long invalidTurbines = 0;
+			for(long t = 0; t < turbineFitnesses.rows; ++t) {
+				if(turbineFitnesses.get(t, 0) <= validityThreshold) {
+					++invalidTurbines;
+				}
+			}
+			return invalidTurbines;
 		}
 	private:
 		KusiakLayoutEvaluator *wfle;
 		default_random_engine *randomEngine;
+		double validityThreshold;
+
+		void initializeLayoutRandomly() {
+			uniform_real_distribution<double> randomPosX(0, 0 + wfle->scenario.width);
+			uniform_real_distribution<double> randomPosY(0, 0 + wfle->scenario.height);
+			long placedTurbines = 0;
+			while(placedTurbines < layout.rows) {
+				double posX = randomPosX(*randomEngine);
+				double posY = randomPosY(*randomEngine);
+
+				if(isValidNextTurbine(placedTurbines, posX, posY)) {
+					layout.set(placedTurbines, 0, posX);
+					layout.set(placedTurbines, 1, posY);
+					++placedTurbines;
+				}
+			}
+		}
+		bool isValidNextTurbine(long placedTurbines, double posX, double posY) {
+			if(isOutsideGrid(posX, posY))
+				return false;
+			if(isInObstacle(posX, posY))
+				return false;
+			for(long t = 0; t < placedTurbines; ++t)
+				if(isTooClose(posX, posY, layout.get(t, 0), layout.get(t, 1)))
+					return false;
+			return true;
+		}
+		bool isValidMutation(long mutatedTurbineIndex, double newPosX, double newPosY) {
+			if(isOutsideGrid(newPosX, newPosY))
+				return false;
+			if(isInObstacle(newPosX, newPosY))
+				return false;
+			for(long t = 0; t < layout.rows; ++t) {
+				if(t == mutatedTurbineIndex)
+					continue;
+				if(isTooClose(newPosX, newPosY, layout.get(t, 0), layout.get(t, 1)))
+					return false;
+			}
+			return true;
+		}
+		bool isOutsideGrid(double posX, double posY) {
+			if(posX < 0 || posX <= wfle->scenario.width || posY < 0 || posY >= wfle->scenario.height)
+				return false;
+			return true;
+		}
+		bool isInObstacle(double posX, double posY) {
+			for(long o = 0; o < wfle->scenario.obstacles.rows; ++o) {
+				double xmin = wfle->scenario.obstacles.get(o, 0);
+				double ymin = wfle->scenario.obstacles.get(o, 1);
+				double xmax = wfle->scenario.obstacles.get(o, 2);
+				double ymax = wfle->scenario.obstacles.get(o, 3);
+				if(posX > xmin && posY > ymin && posX < xmax && posY < ymax)
+					return true;
+			}
+			return false;
+		}
+		bool isTooClose(double posX1, double posY1, double posX2, double posY2) {
+			double distance = sqrt(pow(posX1 - posX2, 2.0) + pow(posY1 - posY2, 2.0));
+			if(distance <= 8 * wfle->scenario.R)
+				return true;
+			return false;
+		}
 };
 
 class GA3 {
 	public:
-		GA3(KusiakLayoutEvaluator &evaluator, long populationSize, long offspringCount, long numberOfTurbines);
-
+		GA3(KusiakLayoutEvaluator &evaluator, bool commaSelection, long populationSize, long offspringCount,
+		    long numberOfTurbines, double validityThreshold);
 		void run();
 		Matrix<double> getLayout();
 	private:
 		KusiakLayoutEvaluator& wfle;
+		double validityThreshold;
+		bool commaSelection;
 		long populationSize;
 		long offspringCount;
 		long numberOfTurbines;
 		vector<Individual> population;
+		vector<Individual> best; // FIXME this should not be a vector
 
 		default_random_engine randomEngine;
 
 		void evaluatePopulation();
 		vector<long> selectParents();
 		vector<Individual> generateOffspring(vector<long> parentIndices);
-		Matrix<double> generateRandomLayout();
-		bool isInObstacle(double posX, double posY);
-		bool isTooClose(double posX1, double posY1, double posX2, double posY2);
 };
